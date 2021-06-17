c8ccab238232e131eca35654cfc86ca5041298f9
Cabelinho <victor.nicacio@usp.br>
2021-06-16 21:40:44 -0300

    Initial commit

---
+++ src/Score.java
@@ -0,0 +1,53 @@
+import java.awt.*;
+
+/**
+	Esta classe representa um placar no jogo. A classe princial do jogo (Pong)
+	instancia dois objeto deste tipo, cada um responsável por gerenciar a pontuação
+	de um player, quando a execução é iniciada.
+*/
+
+public class Score {
+
+	private final String playerId;
+	private int score;
+
+	/**
+		Construtor da classe Score.
+
+		@param playerId uma string que identifica o player ao qual este placar está associado.
+	*/
+
+	public Score(String playerId){
+		this.playerId = playerId;
+		this.score = 0;
+	}
+
+	/**
+		Método de desenho do placar.
+	*/
+
+	public void draw(){
+
+		GameLib.drawText("Player 1: ", 70, GameLib.ALIGN_LEFT);
+		GameLib.drawText("Player 2: ", 70, GameLib.ALIGN_RIGHT);
+	}
+
+	/**
+		Método que incrementa em 1 unidade a contagem de pontos.
+	*/
+
+	public void inc(){
+		this.score++;
+		GameLib.drawText(Integer.toString(getScore()), 80, GameLib.ALIGN_LEFT);
+	}
+
+	/**
+		Método que devolve a contagem de pontos mantida pelo placar.
+
+		@return o valor inteiro referente ao total de pontos.
+	*/
+
+	public int getScore(){
+		return this.score;
+	}
+}

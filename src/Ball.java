8eca476f52e84fa39b94c54c0ae3c8fcf45004a1
Victor <v1c70r1119@proton.me>
2024-02-23 03:39:32 -0300

    refacting

--- src/Ball.java
+++ src/Ball.java
@@ -2,8 +2,8 @@ import java.awt.*;
 import java.util.Random;
 
 /**
-	Esta classe representa a bola usada no jogo. A classe princial do jogo (Pong)
-	instancia um objeto deste tipo quando a execução é iniciada.
+*	Esta classe representa a bola usada no jogo. A classe princial do jogo (Pong)
+*	instancia um objeto deste tipo quando a execução é iniciada.
 */
 
 public class Ball {
@@ -17,16 +17,16 @@ public class Ball {
 	double xDir, yDir;
 
 	/**
-		Construtor da classe Ball. Observe que quem invoca o construtor desta classe define a velocidade da bola 
-		(em pixels por millisegundo), mas não define a direção deste movimento. A direção do movimento é determinada 
-		aleatóriamente pelo construtor.
-
-		@param cx coordenada x da posição inicial da bola (centro do retangulo que a representa).
-		@param cy coordenada y da posição inicial da bola (centro do retangulo que a representa).
-		@param width largura do retangulo que representa a bola.
-		@param height altura do retangulo que representa a bola.
-		@param color cor da bola.
-		@param speed velocidade da bola (em pixels por millisegundo).
+	*	Construtor da classe Ball. Observe que quem invoca o construtor desta classe define a velocidade da bola 
+	*	(em pixels por millisegundo), mas não define a direção deste movimento. A direção do movimento é determinada 
+	*	aleatóriamente pelo construtor.
+	*
+	*	@param cx coordenada x da posição inicial da bola (centro do retangulo que a representa).
+	*	@param cy coordenada y da posição inicial da bola (centro do retangulo que a representa).
+	*	@param width largura do retangulo que representa a bola.
+	*	@param height altura do retangulo que representa a bola.
+	*	@param color cor da bola.
+	*	@param speed velocidade da bola (em pixels por millisegundo).
 	*/
 
 	public Ball(double cx, double cy, double width, double height, Color color, double speed){
@@ -45,20 +45,18 @@ public class Ball {
 
 
 	/**
-		Método chamado sempre que a bola precisa ser (re)desenhada.
+	*	Método chamado sempre que a bola precisa ser (re)desenhada.
 	*/
-
 	public void draw(){
 		GameLib.setColor(getColor());
 		GameLib.fillRect(getCx(), getCy(), getWidth(), getHeight());
 	}
 
 	/**
-		Método chamado quando o estado (posição) da bola precisa ser atualizado.
-		
-		@param delta quantidade de millisegundos que se passou entre o ciclo anterior de atualização do jogo e o atual.
+	*	Método chamado quando o estado (posição) da bola precisa ser atualizado.
+	*	
+	*	@param delta quantidade de millisegundos que se passou entre o ciclo anterior de atualização do jogo e o atual.
 	*/
-
 	public void update(long delta){
 		this.cx += delta * getxDir();
 		if(getxDir() == 0) this.xDir = -getSpeed();
@@ -70,22 +68,20 @@ public class Ball {
 	}
 
 	/**
-		Método chamado quando detecta-se uma colisão da bola com um jogador.
-	
-		@param playerId uma string cujo conteúdo identifica um dos jogadores.
+	*	Método chamado quando detecta-se uma colisão da bola com um jogador.
+	*
+	*	@param playerId uma string cujo conteúdo identifica um dos jogadores.
 	*/
-
 	public void onPlayerCollision(String playerId){
 		if(playerId.equals("Player 1")) this.xDir = getSpeed();
 		else this.xDir = -getSpeed();
 	}
 
 	/**
-		Método chamado quando detecta-se uma colisão da bola com uma parede.
-
-		@param wallId uma string cujo conteúdo identifica uma das paredes da quadra.
+	*	Método chamado quando detecta-se uma colisão da bola com uma parede.
+	*
+	*	@param wallId uma string cujo conteúdo identifica uma das paredes da quadra.
 	*/
-
 	public void onWallCollision(String wallId){
 		if (wallId.equals("Top")) this.yDir = getSpeed();
 		else if (wallId.equals("Bottom")) this.yDir = -getSpeed();
@@ -94,12 +90,11 @@ public class Ball {
 	}
 
 	/**
-		Método que verifica se houve colisão da bola com uma parede.
-
-		@param wall referência para uma instância de Wall contra a qual será verificada a ocorrência de colisão da bola.
-		@return um valor booleano que indica a ocorrência (true) ou não (false) de colisão.
+	*	Método que verifica se houve colisão da bola com uma parede.
+	*
+	*	@param wall referência para uma instância de Wall contra a qual será verificada a ocorrência de colisão da bola.
+	*	@return um valor booleano que indica a ocorrência (true) ou não (false) de colisão.
 	*/
-	
 	public boolean checkCollision(Wall wall){
 		double meia_altura = getHeight()/2;
 		double meia_largura = getWidth()/2;
@@ -142,51 +137,19 @@ public class Ball {
 		return false;
 	}
 
-	/**
-		Método que devolve a coordenada x do centro do retângulo que representa a bola.
-		@return o valor double da coordenada x.
-	*/
-	
-	public double getCx(){
-		return this.cx;
-	}
+	public double getCx() { return this.cx; }
 
-	/**
-		Método que devolve a coordenada y do centro do retângulo que representa a bola.
-		@return o valor double da coordenada y.
-	*/
+	public double getCy() { return this.cy; }
 
-	public double getCy(){
-		return this.cy;
-	}
+	public double getSpeed() { return this.speed; }
 
-	/**
-		Método que devolve a velocidade da bola.
-		@return o valor double da velocidade.
+	public Color getColor() { return this.color; }
 
-	*/
+	public double getWidth() { return this.width; }
 
-	public double getSpeed(){
-		return this.speed;
-	}
+	public double getHeight() { return this.height; }
 
-	public Color getColor() {
-		return this.color;
-	}
+	public double getxDir() { return this.xDir; }
 
-	public double getWidth() {
-		return this.width;
-	}
-
-	public double getHeight() {
-		return this.height;
-	}
-
-	public double getxDir() {
-		return this.xDir;
-	}
-
-	public double getyDir() {
-		return this.yDir;
-	}
+	public double getyDir() { return this.yDir; }
 }
